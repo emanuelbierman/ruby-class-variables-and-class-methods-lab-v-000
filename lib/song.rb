3053fa5a7ff057d8f115e030d5648679e83f2c30
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-21 22:29:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,19 +16,19 @@ class Song
     @@genres << genre
   end
 
-  def count
+  def self.count
     @@count
   end
 
-  def artists
+  def self.artists
     @@artists.uniq
   end
 
-  def genres
+  def self.genres
     @@genres.uniq
   end
 
-  def genre_count
+  def self.genre_count
     @@genres.each do |genre|
       if @@genre_count.has_key?(genre)
         @@genre_count[genre] += 1
@@ -38,7 +38,7 @@ class Song
     end
   end
 
-  def artist_count
+  def self.artist_count
     @@artists.each do |artist|
       if @@artist_count.has_key?(artist)
         @@artist_count[artist] += 1
