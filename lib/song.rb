3db6272bd6f150b87f3b6b3a48808b5e7b0a96ea
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-21 22:02:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,17 @@
 class Song
   attr_accessor :name, :artist, :genre, :count
 
+  @@artists = []
+  @@genres = []
+  @@count = 0
+  @@artist_count = {}
+  @@genre_count = {}
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
   end
 
-  
+
 end
