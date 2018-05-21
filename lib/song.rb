c4ab8c085c34f2fe778c510fb55675d7e9fec3b3
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-21 22:23:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   def genre_count
     @@genres.each do |genre|
       if @@genre_count.has_key?(genre)
-        @@genre_count[genre] =+ 1
+        @@genre_count[genre] += 1
       else
         @@genre_count[genre] = 1
       end
