d29699ec6df31e973a02fc18c960d3fc8550eedc
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-21 22:30:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
         @@genre_count[genre] = 1
       end
     end
-    @@genres
+    @@genre_count
   end
 
   def self.artist_count
@@ -47,6 +47,7 @@ class Song
         @@artist_count[artist] = 1
       end
     end
+    @@artist_count
   end
 
 end
