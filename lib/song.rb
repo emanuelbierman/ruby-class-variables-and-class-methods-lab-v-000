1a6f27dadba4fcf53ca88884497a77577b0b18a9
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-21 22:16:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,13 @@ class Song
   end
 
   def artist_count
-
+    @@artists.each do |artist|
+      if artist in @@artist_count
+        @@artist_count[artist] =+ 1
+      else
+        @@artist_count[artist] = 1
+      end
+    end
   end
 
 end
